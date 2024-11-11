1c0fec283d0bb3504d9a4d50bb253fa7f61e62fd
qinshaoshuai <342297670@qq.com>
2024-11-11 20:30:20 +0800

    create gitee

---
+++ src/main/java/com/qss/MyGongjukuApplication.java
@@ -0,0 +1,13 @@
+package com.qss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGongjukuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGongjukuApplication.class, args);
+    }
+
+}
